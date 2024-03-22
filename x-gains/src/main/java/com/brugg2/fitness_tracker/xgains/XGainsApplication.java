9fbc407fbdf25338e30ca14aa14f2232743c6546
nom <nom@XUL-M1BA.local>
2024-03-22 16:57:27 +0100

    Changed project to a spring boot project

---
+++ x-gains/src/main/java/com/brugg2/fitness_tracker/xgains/XGainsApplication.java
@@ -0,0 +1,13 @@
+package com.brugg2.fitness_tracker.xgains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XGainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XGainsApplication.class, args);
+	}
+
+}
---
+++ x-gains/src/test/java/com/brugg2/fitness_tracker/xgains/XGainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brugg2.fitness_tracker.xgains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XGainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
