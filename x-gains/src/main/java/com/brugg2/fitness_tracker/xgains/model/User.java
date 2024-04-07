5dbdbf3264abb10368a20d37e386e49bed0133c1
nom <nom@XUL-M1BA.local>
2024-04-07 18:04:57 +0200

    added user class and user tests, getters and setter, function to hash password

---
+++ x-gains/src/main/java/com/brugg2/fitness_tracker/xgains/model/User.java
@@ -0,0 +1,133 @@
+package com.brugg2.fitness_tracker.xgains.model;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+public class User {
+
+    private int userID;
+    private String accountType;
+    private String username;
+    private String email;
+    private int salt;
+    private String hashedPassword;
+    private String firstname;
+    private String lastname;
+    //private String birthdate;
+
+    private static final Random RANDOM = new Random();
+
+
+    // Setters
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * Sets the account type of a user. For simplicity the user account type is
+     * specified
+     * with a number. Only the right combination of numbers will result in admin.
+     * Chose -> 12345678 to set admin.
+     * @param accountType all numbers except the special number result in user.
+     */
+    public void setAccountType(int accountType) {
+        if (accountType == 123456789) {
+            this.accountType = "admin";
+        } else {
+            this.accountType = "user";
+        }
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+       String newPassword =  hashPassword(password);
+       this.hashedPassword = newPassword; 
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+
+    // Getters
+    public int getUserID() {
+        return this.userID;
+    }
+
+    public String getAccountType() {
+        return this.accountType;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getHashedPassword() {
+        return this.hashedPassword;
+    }
+
+    public String getFirstname() {
+        return this.firstname;
+    }
+
+    public String getLastname() {
+        return this.lastname;
+    }
+
+
+    /**
+     * Setting the salt for the user. The method is called when using the setter
+     * to set the instance variable hashedPassword.
+     * The salt is only reset when the password is changed.
+     */
+    private void setSalt() {
+        int randomNumber = RANDOM.nextInt() * 100 + RANDOM.nextInt();
+        this.salt = randomNumber;
+    }
+
+
+    /**
+     * Function to hash the passwort. When calling the method a new salt is set for
+     * user instance. The salt and the hashed password can be stored in the database.
+     * Function can be used for setting the password as well as loging in. 
+     * @param password is the password in plain text entered by the user.
+     */
+    public String hashPassword(String password) {
+        setSalt();
+        String saltedPassword = password + this.salt;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] bytes = md.digest(saltedPassword.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            hashedPassword = sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return hashedPassword;
+    }
+
+}
---
+++ x-gains/src/test/java/com/brugg2/fitness_tracker/xgains/model/UserTest.java
@@ -0,0 +1,42 @@
+package com.brugg2.fitness_tracker.xgains.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    /**
+     * Test not fully implemented. Logic how to test not clear.
+     */
+    void hashedPasswordShouldReturnWhat() {
+        User test = new User();
+        test.setPassword("test");
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void testSetAccountType() {
+        User test = new User();
+        test.setAccountType(123456789);
+        assertEquals("admin", test.getAccountType());
+
+        test.setAccountType(1);
+        assertEquals("user", test.getAccountType());
+    }
+
+    @Test
+    void testSetPassword() {
+        User test = new User();
+
+        boolean condition = test.getHashedPassword() == null;
+        assertTrue(condition);
+
+        test.setPassword("test");
+
+        condition = test.getHashedPassword() != null;
+        assertTrue(condition);
+    }
+}
