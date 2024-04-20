12b8dff0ab9eb0eef87acf4710ddf1cdf05229d2
nom <manuel.notter@bluewin.ch>
2024-04-20 19:00:08 +0200

    Added /api/user/account to retrieve account information. Hashed password is retrieved as well - needs to be fixed.

--- x-gains/src/main/java/com/brugg2/fitness_tracker/xgains/controller/UserController.java
+++ x-gains/src/main/java/com/brugg2/fitness_tracker/xgains/controller/UserController.java
@@ -2,6 +2,7 @@ package com.brugg2.fitness_tracker.xgains.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brugg2.fitness_tracker.xgains.model.dao.UserRepository;
 import com.brugg2.fitness_tracker.xgains.model.entity.User;
 import com.brugg2.fitness_tracker.xgains.model.service.UserService;
 
@@ -11,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,18 +22,22 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
+
     /**
      * Method to create a new user using http request and save to database.
      * Input names of the attributes need to be the java class variable names.
-     * @param user is input as a JSON object and converted to a Java object by Spring.
+     * 
+     * @param user is input as a JSON object and converted to a Java object by
+     *             Spring.
      * @return Returns the saved object in the database in JSON format.
      */
     @Autowired
     @PostMapping("/new")
     public ResponseEntity addUser(@RequestBody User user) {
         try {
-           
-            
+
             userService.createUser(user);
 
         } catch (Exception e) {
@@ -44,7 +51,10 @@ public class UserController {
      * Method to remove a new user using http request and save to database.
      * Input names of the attributes need to be the java class variable names.
      * Cannot delete user without fk references (workouts) are deleted as well.
-     * @param user is input as a JSON object and converted to a Java object by Spring.
+     * 
+     * @param user is input as a JSON object and converted to a Java object by
+     *             Spring. Also a field of the user can be passed which will be
+     *             converted to a Json object. E.g. userID = 1.
      * @return Returns the deleted object in the database in JSON format.
      */
     @PostMapping("/delete")
@@ -58,5 +68,27 @@ public class UserController {
         }
         return ResponseEntity.ok(user);
     }
+
+    /**
+     * Method to view user details.
+     * Input is the prinamry key userID.
+     * Also returns the hashed password :D. No bueno.
+     * @param userID int
+     * @return User object in Json format.
+     */
+    @GetMapping("account")
+    public ResponseEntity getAccountDetails(@RequestParam int userID) {
+        try {
+            User user = userService.getUserById(userID);
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            }
+            return ResponseEntity.ok(user);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.toString());
+        }
+    }
+
 
 }
